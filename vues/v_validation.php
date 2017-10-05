aef66e58d424289fcec95578254482b4cc2136b2
laze <laze@G14D0S>
2017-10-05 17:28:31 +0200

    creation de v_validation.php

---
+++ vues/v_validation.php
@@ -0,0 +1,71 @@
+<h3>Fiche de frais de <?php echo $nomUtilisateur." "; echo $prenomUtilisateur." " ?> du mois <?php echo $numMois."-".$numAnnee?> : </h3>
+    <div class="encadre">
+    <p>
+        Etat : <?php echo $libEtat?> depuis le <?php echo $dateModif?> <br>
+    </p>
+  	<table class="listeLegere">
+  	   <caption>Eléments forfaitisés </caption>
+        <tr>
+            <?php
+            foreach ( $lesFraisForfait as $unFraisForfait ) 
+            {
+                $libelle = $unFraisForfait['libelle'];
+                ?>	
+                <th> <?php echo $libelle?></th>
+                <?php
+            }
+            ?>
+	</tr>
+        
+        <tr>
+        <?php
+          foreach (  $lesFraisForfait as $unFraisForfait  ) 
+		  {
+				$quantite = $unFraisForfait['quantite'];
+		?>
+                <td class="qteForfait"><?php echo $quantite?> </td>
+		 <?php
+          }
+		?>
+		</tr>
+    </table>
+  	<table class="listeLegere">
+  	   <caption>Descriptif des éléments hors forfait -<?php echo $nbJustificatifs ?> justificatifs reçus -
+       </caption>
+             <tr>
+                <th class="date">Date</th>
+                <th class="libelle">Libellé</th>
+                <th class='montant'>Montant</th>
+                <th>Reporter frais</th> //ajout possibiliter de reporter
+                <th>Supprimer frais</th> //ajout possibiliter de supprimer
+             </tr>
+        <?php      
+          foreach ( $lesFraisHorsForfait as $unFraisHorsForfait ) 
+		  {
+			$date = $unFraisHorsForfait['date'];
+			$libelle = $unFraisHorsForfait['libelle'];
+			$montant = $unFraisHorsForfait['montant'];
+		?>
+             <tr>
+                <td><?php echo $date ?></td>
+                <td><?php echo $libelle ?></td>
+                <td><?php echo $montant ?></td>
+                <!-- ajout du lien de report -->
+                <td><a href="index.php?uc=validerFrais
+                                      &action=reporter
+                                      &idFraisHorsForfait=<?php echo $unFraisForfait['id'] ?>
+                                      &idVisiteur=<?php echo $idVisiteur ?>
+                                      &mois=<?php echo $mois ?>">Reporter</a></td>
+                <!-- ajout du lien de suppression -->
+                <td><a href="index.php?uc=validerFrais
+                                      &action=supprimer
+                                      &idFraisHorsForfait=<?php echo $unFraisForfait['id'] ?>
+                                      &idVisiteur=<?php echo $idVisiteur ?>
+                                      &mois=<?php echo $mois ?>">Supprimer</a></td>
+             </tr>
+        <?php 
+          }
+		?>
+    </table>
+  </div>
+  </div>
