0bf0d8f6f83ca30fecfe25357a1460b01142c8f8
laze <laze@G14D0S>
2017-10-06 16:09:22 +0200

    mise a jour final de v_modifiCationFraisForfait.php

--- vues/v_modificationFraisForfait.php
+++ vues/v_modificationFraisForfait.php
@@ -1,40 +1,41 @@
-<!-- vue pour modifier la quantite de frais forfaitises -->
 <div id="contenu">
-<h3>Modifier la quantité d'élément forfaitisés</h3>
-
-<form action="index.php?uc=validerFrais&action=appliquerModification" method="post">
-    <!-- tableau des frais forfait -->
-    <table class="listeLegere">
-        
-        <!-- entete du tableau -->
-        <tr>
-    <?php
-            foreach ( $lesFraisForfait as $unFraisForfait ) 
-            {
-                $libelle = $unFraisForfait['libelle'];
-    ?>	
-                <th> <?php echo $libelle?></th>
-    <?php
-            }
-    ?>
-        </tr>
-        
-        <!-- reste du tableau -->
-        <tr>
-    <?php
-            foreach (  $lesFraisForfait as $unFraisForfait  ) 
-            {
-                $quantite = $unFraisForfait['quantite'];
-    ?>
-                <!-- stockage des quantites dans une variable $_post avec comme index idfrais-->
-                <td class="qteForfait">
-                    <input type="text" value="<?php echo $quantite?>" size="15" name="<?php echo $unFraisForfait['idfrais'] ?>" />
-                </td>
-    <?php
-            }
-    ?>
-        </tr>
-    </table>
+    <h2>Modifier la fiche de frais</h2>
     
-    <input type="submit" value="valider" size="20"/>
-</form>
+    <!-- formulaire pour recuperer les quantites -->
+    <form method="POST"  action="index.php?uc=validerFrais&action=appliquerModification">
+        <!--leMois et leVisiteur -->
+        <input type="hidden" value="<?php echo $moisASelectionner?>" name="leMois" />
+        <input type="hidden" value="<?php echo $visiteurASelectionner?>" name="leVisiteur" />
+        
+        
+        <div class="corpsForm">
+          
+            <fieldset>
+                <legend>Eléments forfaitisés</legend>
+                 
+        <?php
+                foreach ($lesFraisForfait as $unFrais)
+                {
+                    $idFrais = $unFrais['idfrais'];
+                    $libelle = $unFrais['libelle'];
+                    $quantite = $unFrais['quantite'];
+	?>
+                <p>
+                    <label for="idFrais"><?php echo $libelle ?></label>
+                    <input type="text" id="idFrais" name="lesFrais[<?php echo $idFrais?>]" size="10" maxlength="5" value="<?php echo $quantite?>" >
+		</p>
+	<?php
+		}
+	?>
+            </fieldset>
+            
+        </div>
+          
+        <div class="piedForm">
+            <p>
+                <input id="ok" type="submit" value="Valider" size="20" />
+                <input id="annuler" type="reset" value="Effacer" size="20" />
+            </p> 
+        </div>
+        
+    </form>
