54c928f39f2464258aedd03dabaeb2ce0a95620f
laze <laze@G14D0S>
2017-10-07 21:02:34 +0200

    création de la vue v_confirmationValidation

---
+++ vues/v_confirmationValidation.php
@@ -0,0 +1,10 @@
+<h3>Confirmation validation : </h3>
+
+<div class="encadre">
+    <p>
+        Enregistrement de la fiche de frais de !!RECUPERER_LE_NOM!! du <?php echo $numMois ?>/<?php echo $numAnnee ?>
+    </p>
+    <!-- bouton de retour à la page d'accueil du comptable -->
+    <button><a href="index.php?uc=connexion&action=valideConnexion" /></button>
+</div>
+
