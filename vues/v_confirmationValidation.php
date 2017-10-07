31cb3430289d233a2d0c18a50e4dd4e89718ecc0
laze <laze@G14D0S>
2017-10-07 21:14:03 +0200

    mise à jour mise en forme

--- vues/v_confirmationValidation.php
+++ vues/v_confirmationValidation.php
@@ -1,10 +1,9 @@
 <h3>Confirmation validation : </h3>
-
-<div class="encadre">
-    <p>
-        Enregistrement de la fiche de frais de !!RECUPERER_LE_NOM!! du <?php echo $numMois ?>/<?php echo $numAnnee ?>
-    </p>
-    <!-- bouton de retour à la page d'accueil du comptable -->
-    <button><a href="index.php?uc=connexion&action=valideConnexion" /></button>
-</div>
-
+<div id='contenu'>
+    <div class="encadre">
+        <p>
+            Enregistrement de la fiche de frais de !!RECUPERER_LE_NOM!! du <?php echo $numMois ?>/<?php echo $numAnnee ?>
+        </p>
+        <!-- bouton de retour à la page d'accueil du comptable -->
+        <a href="index.php?uc=validerFrais&action=choisirMois">Valider une autre fiche</a>
+    </div>
