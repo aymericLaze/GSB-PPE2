761e770374cbd8de1b990b2022b9cba51385d971
laze <laze@G14D0S>
2017-10-10 17:37:45 +0200

    mise a jour affichage

--- vues/v_confirmationValidation.php
+++ vues/v_confirmationValidation.php
@@ -2,7 +2,7 @@
 <div id='contenu'>
     <div class="encadre">
         <p>
-            Enregistrement de la fiche de frais de !!RECUPERER_LE_NOM!! du <?php echo $numMois ?>/<?php echo $numAnnee ?>
+            Enregistrement de la fiche de frais
         </p>
         <!-- bouton de retour à la page d'accueil du comptable -->
         <a href="index.php?uc=selectionnerFicheFrais&action=choisirMois">Valider une autre fiche</a>
