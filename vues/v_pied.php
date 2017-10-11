2ba7dd1b1f3344202ef0b96a4828de08335f71de
laze <laze@G14D0S>
2017-10-11 12:56:20 +0200

    ajout retour a l'accueil

--- vues/v_pied.php
+++ vues/v_pied.php
@@ -1,4 +1,11 @@
-
+<?php
+    if($_SESSION['fonction'] === 'comptable')
+    {
+?>
+        <a href='index.php?uc=selectionnerFicheFrais&action=accueil'>Retour à l'accueil</a>
+<?php
+    }
+?>
   <!-- Division pour le pied de page -->
     
   </body>
