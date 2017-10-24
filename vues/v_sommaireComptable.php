4da00a3fa6dc2fad2988ccb54fac7504068be0ed
laze <laze@G14D0S>
2017-10-24 17:38:27 +0200

    mise à jour du sommaire pour selectionner fiches en cours de payement

--- vues/v_sommaireComptable.php
+++ vues/v_sommaireComptable.php
@@ -16,17 +16,25 @@
     
       </div>  
         <ul id="menuList">
-			<li >
-				  Comptable :<br>
-				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
-			</li>
-           <li class="smenu">
-              <a href="index.php?uc=selectionnerFicheFrais&action=choisirMois" title="Valider fiche de frais ">Valider fiche de frais</a>
-           </li>
-           
- 	   <li class="smenu">
-              <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
-           </li>
+            <li>
+                    Comptable :<br>
+                    <?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
+            </li>
+
+            <!-- lien pour valider une fiche -->
+            <li class="smenu">
+                    <a href="index.php?uc=selectionnerFicheFrais&action=choisirMois" title="Valider fiche de frais ">Valider fiche de frais</a>
+            </li>
+            
+            <!-- lien pour voir fiche frais en payement -->
+            <li class="smenu">
+                <a href="index.php?uc=selectionnerFicheFrais&action=choisirFicheSuiviPayement" title="Suivi de paiement ">Suivi des fiches en paiement</a>
+            </li>
+            
+            <!-- lien pour deconnexion -->
+            <li class="smenu">
+                    <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
+            </li>
          </ul>
         
     </div>
