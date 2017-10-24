1c6846d248dfeadcbc68ed25354b9e0d8dcbb90c
laze <laze@G14D0S>
2017-10-24 18:31:49 +0200

    création de la vue v_selectionFichesEnPayement

---
+++ vues/v_selectionFichesEnPayement.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Vue pour sélectionner une fiche en cours de payement
+ * 
+ * La valeur du select est la concatenation de l'idVisiteur et du mois avec un "/"
+ * 
+ * @author LAZE Aymeric
+ */
+
+if ($nbFiche != 0){
+
+?>
+<div id="contenu">
+    <h2>Fiches de frais</h2>
+    <h3>Fiche à sélectionner : </h3>
+    <form action="" method="post">
+        <div class="corpsForm">
+
+            <p>
+
+                <label for="lstMois" accesskey="n">Fiches : </label>
+                <select id="listMois" name="lstFiche">
+                    <?php
+                    foreach ($lesFicheEnPayement as $uneFiche) {
+                        $visiteur = $uneFiche['visiteur'];
+                        $mois = $uneFiche['mois'];
+                        $identite = $uneFiche['identite'];
+                        $numAnnee = $uneFiche['numAnnee'];
+                        $numMois = $uneFiche['numMois'];
+                    ?>
+                            <option value="<?php echo $visiteur. "/" .$mois ?>"><?php echo $numMois . "/" . $numAnnee." - ". $identite ?> </option>
+                    <?php
+                    }
+                    ?>    
+
+                </select>
+            </p>
+        </div>
+        <div class="piedForm">    
+            <p>
+                <input id="ok" type="submit" value="Valider" size="20" />
+                <input id="annuler" type="reset" value="Effacer" size="20" />
+            </p> 
+        </div>
+
+    </form>
+<?php
+} else {
+    ?>
+      <h3>Aucune fiche en cours de payement</h3>
+      <?php
+}
+?>
