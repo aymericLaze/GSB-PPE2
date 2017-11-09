dd3a1d7ad9f648b63cb827649f985774427e9784
laze <laze@G14D0S>
2017-11-09 12:48:34 +0100

    ajout de getMoisTextuelle

--- include/fct.inc.php
+++ include/fct.inc.php
@@ -234,4 +234,30 @@ function incrementerMois($mois)
     
     return $nouveauMois;
 }
+
+/**
+ * retourne le nom du mois textuelle
+ * 
+ * @param str $numMois
+ * @return string
+ * 
+ * @author LAZE Aymeric
+ */
+function getMoisTextuelle($numMois) {
+    $listMois = array(  '01'=>'Janvier',
+                        '02'=>'Février',
+                        '03'=>'Mars',
+                        '04'=>'Avril',
+                        '05'=>'Mai',
+                        '06'=>'Juin',
+                        '07'=>'Juillet',
+                        '08'=>'Août',
+                        '09'=>'Septembre',
+                        '10'=>'Octobre',
+                        '11'=>'Novembre',
+                        '12'=>'Decembre'
+                    );
+    
+    return $listMois[$numMois];
+}
 ?>
