ecafe5cadf4716ae97bfeb2854b305bc16f02777
laze <laze@G14D0S>
2018-04-26 14:49:59 +0200

    mise en forme du code de la vue de connexion

--- vues/commun/v_connexion.php
+++ vues/commun/v_connexion.php
@@ -1,20 +1,16 @@
 ﻿<div id="contenu">
-      <h2>Identification utilisateur</h2>
+    <h2>Identification utilisateur</h2>
 
-
-<form method="POST" action="index.php?uc=connexion&action=valideConnexion">
-   
-    
-			<p>
-       <label for="nom">Login*</label>
-       <input id="login" type="text" name="login"  size="30" maxlength="45">
-      </p>
-			<p>
-				<label for="mdp">Mot de passe*</label>
-			  <input id="mdp"  type="password"  name="mdp" size="30" maxlength="45">
-      </p>
-         <input type="submit" value="Valider" name="valider">
-      </p>
-</form>
-
+    <form method="POST" action="index.php?uc=connexion&action=valideConnexion">
+        <p>
+            <label for="nom">Login*</label>
+            <input id="login" type="text" name="login"  size="30" maxlength="45">
+        </p>
+	
+        <p>
+            <label for="mdp">Mot de passe*</label>
+            <input id="mdp"  type="password"  name="mdp" size="30" maxlength="45">
+        </p>
+        <input type="submit" value="Valider" name="valider">
+    </form>
 </div>
